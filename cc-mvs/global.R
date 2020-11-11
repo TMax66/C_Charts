bbc944314145556e3523649092a533f9f7c889f1
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-11-11 16:13:07 +0100

    update

--- cc-mvs/global.R
+++ cc-mvs/global.R
@@ -1,23 +1,28 @@
-library(shiny)
-library(datasets)
-library(qicharts2)
-library(ggplot2)
-library(plotly)
-library(googlesheets)
-library(dplyr)
-library(lubridate)
-library(shinycssloaders)
-library(shinyjs)
+library("shiny")
+library("datasets")
+library("qicharts2")
+library("tidyverse")
+library("plotly")
+library("googledrive")
+# library(dplyr)
+library("lubridate")
+library("shinycssloaders")
+library("shinyjs")
 
-########################
-#token <- gs_auth(cache = FALSE)
-#gd_token()
-# saveRDS(token, file = "googlesheets_token.rds")
-gs_auth(token = "googlesheets_token.rds")
-suppressMessages(gs_auth(token = "googlesheets_token.rds", verbose = FALSE))
+###IL CODICE SEGUENTE SI FA SOLO LA PRIMA VOLTA CHE SI ACCEDE AL DRIVE DI GOOGLE
+# options(gargle_oauth_cache = ".secrets")
+# gargle::gargle_oauth_cache()
+# drive_auth()
+# list.files(".secrets/")
 
-ccsiero <-gs_title("bgccsierologia")
-ccmicro <-gs_title("ccmicrobiologia")
-
+# options(
+#   gargle_oauth_cache = ".secrets",
+#   gargle_oauth_email = TRUE
+# )
+# drive_auth()
+# sheets_auth(token = drive_token())
+#   mydrive<-drive_find(type = "spreadsheet") 
+# id<-mydrive[1,2]
+# dati<-read_sheet(id$id)
 
 
